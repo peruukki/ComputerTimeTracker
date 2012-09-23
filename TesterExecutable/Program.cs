e39131b4615f87458c5f61b1ec306d82adb4394d
Harri Lehtola <peruukki@hotmail.com>
2012-09-23 19:07:25 +0300

    Add executable tester
    Added an executable tester project that enables running the unit tests in
    the debugger.

---
+++ TesterExecutable/Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ComputerTimeTracker;
+using TesterNUnit;
+
+namespace TesterExecutable
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Create wanted testers
+      Tester tester = new Tester();
+      tester.Init();
+
+      // Execute wanted tests
+    }
+  }
+}
