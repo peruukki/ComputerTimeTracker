f5a509e7e02c2e560e9fd1383e06080e428f6ff8
Harri Lehtola <peruukki@hotmail.com>
2012-09-25 20:35:14 +0300

    Add title labels
    Added labels above all "columns" to tell what they mean.

--- Application/TimeReport.Designer.cs
+++ Application/TimeReport.Designer.cs
@@ -41,6 +41,10 @@ namespace ComputerTimeTracker
       this._lblPeriodDuration2 = new System.Windows.Forms.Label();
       this._chkPeriod1 = new System.Windows.Forms.CheckBox();
       this._chkPeriod2 = new System.Windows.Forms.CheckBox();
+      this._LblEvent = new System.Windows.Forms.Label();
+      this._LblTime = new System.Windows.Forms.Label();
+      this._LblDuration = new System.Windows.Forms.Label();
+      this._LblWork = new System.Windows.Forms.Label();
       this._pnlPeriod1.SuspendLayout();
       this._pnlPeriod2.SuspendLayout();
       this.SuspendLayout();
@@ -49,7 +53,7 @@ namespace ComputerTimeTracker
       // 
       this._lblTimeStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
       this._lblTimeStart.AutoSize = true;
-      this._lblTimeStart.Location = new System.Drawing.Point(120, 15);
+      this._lblTimeStart.Location = new System.Drawing.Point(120, 30);
       this._lblTimeStart.Name = "_lblTimeStart";
       this._lblTimeStart.Size = new System.Drawing.Size(57, 14);
       this._lblTimeStart.TabIndex = 0;
@@ -59,7 +63,7 @@ namespace ComputerTimeTracker
       // 
       this._lblTextStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
       this._lblTextStart.AutoSize = true;
-      this._lblTextStart.Location = new System.Drawing.Point(180, 15);
+      this._lblTextStart.Location = new System.Drawing.Point(180, 30);
       this._lblTextStart.Name = "_lblTextStart";
       this._lblTextStart.Size = new System.Drawing.Size(83, 14);
       this._lblTextStart.TabIndex = 1;
@@ -69,7 +73,7 @@ namespace ComputerTimeTracker
       // 
       this._btnOk.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
       this._btnOk.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-      this._btnOk.Location = new System.Drawing.Point(107, 90);
+      this._btnOk.Location = new System.Drawing.Point(107, 105);
       this._btnOk.Name = "_btnOk";
       this._btnOk.Size = new System.Drawing.Size(75, 23);
       this._btnOk.TabIndex = 2;
@@ -81,7 +85,7 @@ namespace ComputerTimeTracker
       // 
       this._lblTimeWork.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
       this._lblTimeWork.AutoSize = true;
-      this._lblTimeWork.Location = new System.Drawing.Point(120, 60);
+      this._lblTimeWork.Location = new System.Drawing.Point(120, 75);
       this._lblTimeWork.Name = "_lblTimeWork";
       this._lblTimeWork.Size = new System.Drawing.Size(57, 14);
       this._lblTimeWork.TabIndex = 3;
@@ -91,7 +95,7 @@ namespace ComputerTimeTracker
       // 
       this._lblTextWork.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
       this._lblTextWork.AutoSize = true;
-      this._lblTextWork.Location = new System.Drawing.Point(180, 60);
+      this._lblTextWork.Location = new System.Drawing.Point(180, 75);
       this._lblTextWork.Name = "_lblTextWork";
       this._lblTextWork.Size = new System.Drawing.Size(64, 14);
       this._lblTextWork.TabIndex = 4;
@@ -101,7 +105,7 @@ namespace ComputerTimeTracker
       // 
       this._lblTextCurrent.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
       this._lblTextCurrent.AutoSize = true;
-      this._lblTextCurrent.Location = new System.Drawing.Point(180, 35);
+      this._lblTextCurrent.Location = new System.Drawing.Point(180, 50);
       this._lblTextCurrent.Name = "_lblTextCurrent";
       this._lblTextCurrent.Size = new System.Drawing.Size(76, 14);
       this._lblTextCurrent.TabIndex = 6;
@@ -111,7 +115,7 @@ namespace ComputerTimeTracker
       // 
       this._lblTimeCurrent.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
       this._lblTimeCurrent.AutoSize = true;
-      this._lblTimeCurrent.Location = new System.Drawing.Point(120, 35);
+      this._lblTimeCurrent.Location = new System.Drawing.Point(120, 50);
       this._lblTimeCurrent.Name = "_lblTimeCurrent";
       this._lblTimeCurrent.Size = new System.Drawing.Size(57, 14);
       this._lblTimeCurrent.TabIndex = 5;
@@ -122,7 +126,7 @@ namespace ComputerTimeTracker
       this._pnlPeriod1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
       this._pnlPeriod1.BackColor = System.Drawing.Color.Green;
       this._pnlPeriod1.Controls.Add(this._lblPeriodDuration1);
-      this._pnlPeriod1.Location = new System.Drawing.Point(37, 22);
+      this._pnlPeriod1.Location = new System.Drawing.Point(37, 37);
       this._pnlPeriod1.Name = "_pnlPeriod1";
       this._pnlPeriod1.Size = new System.Drawing.Size(75, 21);
       this._pnlPeriod1.TabIndex = 7;
@@ -144,7 +148,7 @@ namespace ComputerTimeTracker
       this._pnlPeriod2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
       this._pnlPeriod2.BackColor = System.Drawing.Color.YellowGreen;
       this._pnlPeriod2.Controls.Add(this._lblPeriodDuration2);
-      this._pnlPeriod2.Location = new System.Drawing.Point(37, 42);
+      this._pnlPeriod2.Location = new System.Drawing.Point(37, 57);
       this._pnlPeriod2.Name = "_pnlPeriod2";
       this._pnlPeriod2.Size = new System.Drawing.Size(75, 21);
       this._pnlPeriod2.TabIndex = 8;
@@ -164,7 +168,7 @@ namespace ComputerTimeTracker
       // _chkPeriod1
       // 
       this._chkPeriod1.AutoSize = true;
-      this._chkPeriod1.Location = new System.Drawing.Point(17, 26);
+      this._chkPeriod1.Location = new System.Drawing.Point(17, 41);
       this._chkPeriod1.Name = "_chkPeriod1";
       this._chkPeriod1.Size = new System.Drawing.Size(15, 14);
       this._chkPeriod1.TabIndex = 9;
@@ -174,12 +178,56 @@ namespace ComputerTimeTracker
       // _chkPeriod2
       // 
       this._chkPeriod2.AutoSize = true;
-      this._chkPeriod2.Location = new System.Drawing.Point(17, 46);
+      this._chkPeriod2.Location = new System.Drawing.Point(17, 61);
       this._chkPeriod2.Name = "_chkPeriod2";
       this._chkPeriod2.Size = new System.Drawing.Size(15, 14);
       this._chkPeriod2.TabIndex = 10;
       this._chkPeriod2.UseVisualStyleBackColor = true;
       this._chkPeriod2.Visible = false;
+      // 
+      // _LblEvent
+      // 
+      this._LblEvent.AutoSize = true;
+      this._LblEvent.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this._LblEvent.ForeColor = System.Drawing.SystemColors.ControlDark;
+      this._LblEvent.Location = new System.Drawing.Point(180, 10);
+      this._LblEvent.Name = "_LblEvent";
+      this._LblEvent.Size = new System.Drawing.Size(35, 13);
+      this._LblEvent.TabIndex = 11;
+      this._LblEvent.Text = "Event";
+      // 
+      // _LblTime
+      // 
+      this._LblTime.AutoSize = true;
+      this._LblTime.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this._LblTime.ForeColor = System.Drawing.SystemColors.ControlDark;
+      this._LblTime.Location = new System.Drawing.Point(120, 10);
+      this._LblTime.Name = "_LblTime";
+      this._LblTime.Size = new System.Drawing.Size(29, 13);
+      this._LblTime.TabIndex = 12;
+      this._LblTime.Text = "Time";
+      // 
+      // _LblDuration
+      // 
+      this._LblDuration.AutoSize = true;
+      this._LblDuration.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this._LblDuration.ForeColor = System.Drawing.SystemColors.ControlDark;
+      this._LblDuration.Location = new System.Drawing.Point(50, 10);
+      this._LblDuration.Name = "_LblDuration";
+      this._LblDuration.Size = new System.Drawing.Size(48, 13);
+      this._LblDuration.TabIndex = 13;
+      this._LblDuration.Text = "Duration";
+      // 
+      // _LblWork
+      // 
+      this._LblWork.AutoSize = true;
+      this._LblWork.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this._LblWork.ForeColor = System.Drawing.SystemColors.ControlDark;
+      this._LblWork.Location = new System.Drawing.Point(6, 10);
+      this._LblWork.Name = "_LblWork";
+      this._LblWork.Size = new System.Drawing.Size(37, 13);
+      this._LblWork.TabIndex = 14;
+      this._LblWork.Text = "Work?";
       // 
       // TimeReport
       // 
@@ -187,7 +235,11 @@ namespace ComputerTimeTracker
       this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
       this.CancelButton = this._btnOk;
-      this.ClientSize = new System.Drawing.Size(284, 125);
+      this.ClientSize = new System.Drawing.Size(284, 140);
+      this.Controls.Add(this._LblWork);
+      this.Controls.Add(this._LblDuration);
+      this.Controls.Add(this._LblTime);
+      this.Controls.Add(this._LblEvent);
       this.Controls.Add(this._chkPeriod2);
       this.Controls.Add(this._chkPeriod1);
       this.Controls.Add(this._pnlPeriod2);
@@ -228,6 +280,10 @@ namespace ComputerTimeTracker
     private System.Windows.Forms.Label _lblPeriodDuration2;
     private System.Windows.Forms.CheckBox _chkPeriod1;
     private System.Windows.Forms.CheckBox _chkPeriod2;
+    private System.Windows.Forms.Label _LblEvent;
+    private System.Windows.Forms.Label _LblTime;
+    private System.Windows.Forms.Label _LblDuration;
+    private System.Windows.Forms.Label _LblWork;
   }
 }
 
